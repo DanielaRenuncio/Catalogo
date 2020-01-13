1a3589f363584cc3b1861151348e7a819163b129
Daniela Renuncio <danielarenuncio@gmail.com>
2020-01-13 18:15:47 -0300

    Produtos na View[Fix #1]

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,12 +3,20 @@
 namespace Code\Controller;
 
 use Code\View\View;
+use Code\Entity\Product;
 
 class HomeController 
 {
     public function index()
     {
-        $view = new View('site/index.phtml');       
+       //preciso para na instanciação desta classe um parâmetro que 
+       //é a conexão PDO
+       $pdo = new \PDO('mysql:dbname=formacao_php;host:127.0.0.1','root','maxfono');
+        $products = new Product($pdo);
+       // var_dump($products->findAll());die;
+        $view = new View('site/index.phtml'); 
+        //estou mandando os produtos para a view
+        $view->products = $products->findAll();
         return $view->render();
     }
 }
